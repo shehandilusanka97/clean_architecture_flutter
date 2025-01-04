8205958da8114d890d8852a1ad7d4316e135eb3e
shehandilusanka97 <dilumzsanox9@gmail.com>
2025-01-05 04:30:31 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:clean_architecture/application/core/services/theme_service.dart';
+import 'package:clean_architecture/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        debugShowCheckedModeBanner: false,
+        darkTheme: AppTheme.darkTheme,
+        home: const Scaffold(),
+      );
+    });
+  }
+}
+
